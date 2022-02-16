7e2e5146e6e4aeef2ac9e951057364397ae3e964
Jon Goldberg <jon@megaphonetech.com>
2022-02-16 14:21:57 -0500

    initial commit

---
+++ requiredduration.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once 'requiredduration.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function requiredduration_civicrm_config(&$config) {
+  _requiredduration_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function requiredduration_civicrm_xmlMenu(&$files) {
+  _requiredduration_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function requiredduration_civicrm_install() {
+  return _requiredduration_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function requiredduration_civicrm_uninstall() {
+  return _requiredduration_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function requiredduration_civicrm_enable() {
+  return _requiredduration_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function requiredduration_civicrm_disable() {
+  return _requiredduration_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function requiredduration_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _requiredduration_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function requiredduration_civicrm_managed(&$entities) {
+  return _requiredduration_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function requiredduration_civicrm_caseTypes(&$caseTypes) {
+  _requiredduration_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function requiredduration_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _requiredduration_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+function requiredduration_civicrm_validateForm( $formName, &$fields, &$files, &$form, &$errors ) {
+  if ($formName == 'CRM_Case_Form_Activity') {
+    if (isset($fields['duration']) && $fields['duration'] == '' ) {
+      $errors['duration'] = ts( 'Duration cannot be blank.' );
+    }
+  }
+}
